fff17168520cda151db394c92ab4390c6dded3e3
zarakhanna <zarakhanna@gmail.com>
2020-07-05 06:23:30 +0000

    Added login/register pages by copying from the finance submission
    Still need to change the title.

--- application.py
+++ application.py
@@ -79,16 +79,15 @@ def login():
         elif not request.form.get("password"):
             return apology("must provide password", 403)
 
-        # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username",
-                          username=request.form.get("username"))
-
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+        rows = db.execute("SELECT * FROM users WHERE (username = :username);",
+                          username=request.form.get("username"),
+                         )
+        if len(rows) == 0 or (rows and check_password_hash(rows[0]["hash"], request.form.get("password")) == False):
+            return apology("Invalid username/password")
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
+        session["user_name"] = rows[0]["username"]
 
         # Redirect user to home page
         return redirect("/")
@@ -96,6 +95,7 @@ def login():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
+
 
 
 @app.route("/logout")
@@ -118,8 +118,42 @@ def quote():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    """Register user"""
-    return apology("TODO")
+    #TODO = DEFINE HASH
+# Forget any user_id
+    session.clear()
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+        # Ensure password was confirmed
+        elif not request.form.get("confirmation"):
+            return apology("must provide confirmation", 403)# Ensure password was confirmed
+        elif not request.form.get("confirmation") == request.form.get("password"):
+            return apology("must confirm password properly", 403)
+       # elif not len(request.form.get("password")) > 5:
+           # return apology("you must have a longer password", 403)
+                # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # if username exists
+        if len(rows) > 0:
+            return apology("Username already taken", 403)
+        vals = [request.form.get("username"), generate_password_hash(request.form.get("password"))]
+        user_id = db.execute("INSERT INTO users (username, hash) VALUES (%s, %s)", vals)
+        # return render_template("login.html")
+        session["user_id"] = user_id
+        session["user_name"] = vals[0]
+        return redirect("/") # Redirect user to home page
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
 
 
 @app.route("/sell", methods=["GET", "POST"])
